8bdb2e672ec712db15dcae20014029efdfc8c1e9
ACortex <53233006+ArturCortez@users.noreply.github.com>
2023-03-13 18:09:08 +0000

    uploaded some imagefiles, created playerRef

--- GameWebApp/js/app.js
+++ GameWebApp/js/app.js
@@ -4,18 +4,36 @@ function randomFromArray(array){
 }
 
 function getKeyString(x, y){
-    return '${x}x${y}';
+    return `${x}x${y}`;
 }
 
 (function(){
+
+    let playerId;
+    let playerRef;
 
     firebase.auth().onAuthStateChanged((user) => {
         console.log(user)
         if(user){
             //you're logged in
+            playerId = user.uid;
+            playerRef = firebase.database().ref(`players/${playerId}`);
         } else { //this function returns null if user logged out
             //you're logged out
         }
+
+        playerRef.set({
+            id: playerId,
+            name: "Player",
+            direction: "right",
+            color: "black",
+            x: 3,
+            y: 3,
+            coins: 0,
+
+
+        })
+
     });
 
     firebase.auth().signInAnonymously().catch((error) => {
